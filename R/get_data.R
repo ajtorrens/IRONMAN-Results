36df017015360a5d1b8541686f6b3f346cd0eaba
Mike Smith <grimbough@gmail.com>
2019-07-20 23:13:13 +0200

    Create package

--- R/get_data.R
+++ R/get_data.R
@@ -1,24 +1,25 @@
-library(magrittr)
-library(stringr)
-library(rvest)
-library(dplyr)
-library(lubridate)
 
-getTablePage <- function(race, year, page) {
+#' @importFrom dplyr mutate_all
+#' @importFrom rvest html_node html_table
+#' @importFrom xml2 read_html
+.getTablePage <- function(race, year, page) {
     url <- paste0("http://eu.ironman.com/triathlon/coverage/athlete-tracker.aspx?",
                   "race=", race,
                   "&y=",   year, 
                   "&p=",   page)
-    tab <- read_html(url) %>%
+    tab <- xml2::read_html(url) %>%
         rvest::html_node(xpath = "//table") %>% 
         html_table() %>%
         mutate_all(as.character)
     
-    tab
+    return(tab)
 }
 
-
-getNumPages <- function(race, year) {
+#' @importFrom rvest html_node 
+#' @importFrom xml2 read_html
+#' @importFrom stringr str_extract_all str_remove
+#' @import magrittr
+.getNumPages <- function(race, year) {
     
     url <- paste0("http://eu.ironman.com/triathlon/coverage/athlete-tracker.aspx?",
                   "race=", race,
@@ -46,11 +47,36 @@ getNumPages <- function(race, year) {
     
 }
 
+#' @importFrom dplyr mutate
+.classifyDNF <- function(results_table) {
+    
+    tmp <- results_table %>% 
+        mutate(Status = ifelse((Status == "DNF" & is.na(Run)), "DNF - Run", Status)) %>%
+        mutate(Status = ifelse((grepl("^DNF", Status) & is.na(Bike)), "DNF - Bike", Status))
+    
+    ## sometimes a swim is cancelled, so check before doing this
+    if(!all(is.na(tmp$Swim))) {
+        tmp <- tmp %>%
+            mutate(Status = ifelse((grepl("^DNF", Status) & is.na(Swim)), "DNF - Swim", Status))
+    } 
+    
+    res <- tmp %>%
+        mutate(Status = factor(Status, 
+                               levels = c("FINISHED", "DNF - Run", "DNF - Bike", "DNF - Swim", "DQ", "DNS")))
+    
+    return(res)
+    
+}
+
+#' @importFrom dplyr bind_rows as_tibble arrange
+#' @importFrom lubridate hms as.period
+#' @importFrom pbapply pblapply
+#' @export
 getResultsTable <- function(race, year) {
     
-    n_pages <- getNumPages(race, year)
+    n_pages <- .getNumPages(race, year)
     
-    full_tab <- pbapply::pblapply(seq_len(n_pages), getTablePage, race = race, year = year) %>%
+    full_tab <- pbapply::pblapply(seq_len(n_pages), .getTablePage, race = race, year = year) %>%
         dplyr::bind_rows() %>%
         as_tibble() %>%
         mutate(Status = ifelse(grepl("^[0-9]", Finish), yes = "FINISHED", no = Finish),
@@ -66,6 +92,8 @@ getResultsTable <- function(race, year) {
                    Finish = as.period(hms(Finish))) 
     )
     
-    full_tab <- classifyDNF(full_tab) %>%
+    full_tab <- .classifyDNF(full_tab) %>%
         arrange(Status, Finish)
+    
+    return(full_tab)
 }
--- R/plotting.R
+++ R/plotting.R
@@ -1,11 +1,14 @@
-library(ggplot2)
+
 
 ## finishing times
-ggplot(full_tab, aes(x = as.numeric(Finish))) + 
-    geom_histogram(binwidth = 300) + 
-    scale_x_time()
+#ggplot(full_tab, aes(x = as.numeric(Finish))) + 
+#    geom_histogram(binwidth = 300) + 
+#    scale_x_time()
 
-
+#' @importFrom dplyr group_by summarise mutate
+#' @importFrom forcats fct_relevel
+#' @import ggplot2
+#' @export
 plotDropOutRate <- function(results_table) {
     
     tab <- results_table %>% 
@@ -16,26 +19,39 @@ plotDropOutRate <- function(results_table) {
         
     
     ggplot(tab, aes(x = Status, y = percentage)) + 
-        geom_bar(stat = "identity") +
+        geom_bar(stat = "identity", fill = "skyblue") +
         theme_bw()
 }
 
-classifyDNF <- function(results_table) {
+.plotLeg <- function(results_table, leg = "Finish") {
     
-    tmp <- results_table %>% 
-        mutate(Status = ifelse((Status == "DNF" & is.na(Run)), "DNF - Run", Status)) %>%
-        mutate(Status = ifelse((grepl("^DNF", Status) & is.na(Bike)), "DNF - Bike", Status))
-    
-    ## sometimes a swim is cancelled, so check before doing this
-    if(!all(is.na(tmp$Swim))) {
-        tmp <- tmp %>%
-            mutate(Status = ifelse((grepl("^DNF", Status) & is.na(Swim)), "DNF - Swim", Status))
-    } 
-    
-    res <- tmp %>%
-        mutate(Status = factor(Status, 
-                               levels = c("FINISHED", "DNF - Run", "DNF - Bike", "DNF - Swim", "DQ", "DNS")))
-    
-    return(res)
+    ggplot(results_table %>% 
+               filter( !is.na( .data[[leg]] ) ),
+           aes_(x = as.name(leg))) +
+        geom_density(fill = "skyblue") +
+        scale_x_time() + 
+        theme_classic()
     
 }
+
+plotSwimTimes <- function(results_table) {
+    .plotLeg(results_table, leg = "Swim")
+}
+plotBikeTimes <- function(results_table) {
+    .plotLeg(results_table, leg = "Bike")
+}
+plotRunTimes <- function(results_table) {
+    .plotLeg(results_table, leg = "Run")
+}
+plotFinishTimes <- function(results_table) {
+    .plotLeg(results_table, leg = "Finish")
+}
+
+
+
+#ggplot(results_table %>% 
+    #        filter( !is.na( .data[[leg]] ) ),
+    #    aes_(x = as.name(leg))) +
+    # geom_histogram(binwidth = 600, fill = "skyblue") +
+    # scale_x_time() + 
+    # theme_classic() 
